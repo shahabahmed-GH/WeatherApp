79e508b1dd23d3c2613ae47a4461e910f7f1cc69
Shahab976 <shahab@97solutions.com>
2026-05-19 02:13:03 +0500

    First Push - WeatherApp

---
+++ WeatherApp/Models/WeatherModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApp.Models
+{
+    public class WeatherModel
+    {
+        public string cityName {  get; set; }
+        public float tempC {  get; set; }
+        public float tempF { get; set; }
+        public string error { get; set; }
+    }
+}
